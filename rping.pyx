a33a310ccafe31be768c9d9394e9f3a834a98f66
oscarterah <oscarterah@gmail.com>
2020-09-13 01:30:24 +0300

    added a stop and start

--- rping.pyx
+++ rping.pyx
@@ -1,14 +1,14 @@
 import time
 import os
 
-def net(int k, int n, int m):
+def net(int l, int k, int q, int n, int r, int m):
     y = []
 
     cdef int i, j, p
 
-    for p in range(k):
-        for i in range(n):
-            for j in range(m):
+    for p in range(l, k):
+        for i in range(q, n):
+            for j in range(r, m):
                 hostname = '104.{0}.{1}.{2}'.format(p, i, j)
                 response = os.system("ping -c 1 " + hostname)
                 if response == 0:
